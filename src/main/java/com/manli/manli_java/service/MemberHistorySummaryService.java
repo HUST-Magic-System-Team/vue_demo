8f494c8104dde50ca92b1d76d37ba323e0e444f6
wuchunlin <1339472610@qq.com>
2020-03-23 06:25:24 +0800

    secrond commit

--- src/main/java/com/manli/manli_java/service/MemberHistorySummaryService.java
+++ src/main/java/com/manli/manli_java/service/MemberHistorySummaryService.java
@@ -1,4 +1,24 @@
 package com.manli.manli_java.service;
 
+import com.manli.manli_java.eenum.ErrorCodeEnum;
+import com.manli.manli_java.model_auto.MemberHistorySummaryEntity;
+import com.manli.manli_java.repository.MemberHistorySummaryRepository;
+import com.manli.manli_java.util.ResultBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberHistorySummaryService {
+    @Autowired
+    MemberHistorySummaryRepository memberHistorySummaryRepository;
+    @Autowired
+    MemberService memberService;
+    public ResultBean getList(Integer userId){
+        if (memberService.isMember(userId)){
+
+        }else {
+            return new ResultBean(ErrorCodeEnum.NOT_MEMBER);
+        }
+
+    }
 }
