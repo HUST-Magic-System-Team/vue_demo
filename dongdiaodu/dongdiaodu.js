5753dd5aa9528df305edf1e0e4d8ed816a7f711d
wuchunlin <1339472610@qq.com>
2020-07-08 22:47:00 +0800

    完善动画效果

--- dongdiaodu/dongdiaodu.js
+++ dongdiaodu/dongdiaodu.js
@@ -12,5 +12,22 @@ $(function() {
 		$vi.slideUp(500);
 		$video.pause()
 
-	})
+	});
+	
+	$(window).scroll(function() {
+	  if ($(window).scrollTop() > 400) {
+	    $("#scrollUp").fadeIn(200);
+	  } else {
+	    $("#scrollUp").fadeOut(200);
+	  }
+	});
+	    //点击返回顶部500ms的滑动效果
+   $('#scrollUp').on('click',function () {
+	       $('html,body').animate(
+		   {
+			   scrollTop:0,
+		   },
+		   1500,"easeInOutExpo");
+	   })
+
 })
