2e7d21c147e4ceec8c5da1ef540c7160769a3462
CC <CC@qq.com>
2020-07-18 19:56:47 +0800

    Signed-off-by: CC <CC@qq.com>

--- README.md
+++
@@ -1,7 +0,0 @@
-# frontProject
-
-模仿小米网站项目预览：http://120.79.175.19:8080/mi/mi.html
-
-
-
-懂调度网站预览：http://120.79.175.19:8080/dongdiaodu/index.html
--- src/main/java/com/manli/manli_java/util/TupleOne.java
+++
@@ -1,9 +0,0 @@
-package com.manli.manli_java.util;
-
-public class TupleOne<A> {
-    public final A first;
-
-    public TupleOne(A first) {
-        this.first = first;
-    }
-}
