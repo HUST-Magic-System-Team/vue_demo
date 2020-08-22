86872f92bf650985d59936e10a2826e221c52819
今天吃饭了吗 <63800973+Daz-ddd@users.noreply.github.com>
2020-08-22 23:10:37 +0800

    Add files via upload

---
+++ vuedemo_Zym/src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+	state: {
+		products: [{
+				name: 'apple',
+				price: '2'
+			},
+			{
+				name: 'banana',
+				price: '3'
+			},
+			{
+				name: 'pear',
+				price: '4'
+			},
+			{
+				name: 'melon',
+				price: '5'
+			},
+		]
+	},
+	getters: {
+		changeProduct: (state) => {
+			return state.products.map(val => {
+				return {
+					name: '**' + val.name + '--',
+					price: val.price * 2
+				}
+			});
+		}
+	}
+})
